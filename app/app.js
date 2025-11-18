80236c4d6a81fa2f46ba6c931d5879c2692e3f8d
hariom lodha <hlodha786@gmail.com>
2025-11-18 03:00:56 -0500

    Added About page, updated index, app.js and config for feature branch

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
 console.log("Hello from DevOpsProject!");
 console.log("This is a new change from dev branch.");
 
+function showFeatureMessage() {
+    console.log("Feature branch: New About Page Added.");
+}
+
+showFeatureMessage();
+
+
